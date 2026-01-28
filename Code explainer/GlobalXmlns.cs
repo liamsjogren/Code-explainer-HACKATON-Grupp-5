b26cec6f50c3530d002862da70fba1ed32c73bcc
Liam Sjögren <liamsjogren09@gmail.com>
2026-01-28 13:22:49 +0100

    Add project files.

---
+++ Code explainer/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Code_explainer")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Code_explainer.Pages")]
---
+++ Code explainer/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace Code_explainer
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnMenuClicked(object sender, EventArgs e)
+        {
+            StatusLabel.Text = "Menu clicked (placeholder)";
+        }
+
+        private void OnPlayClicked(object sender, EventArgs e)
+        {
+            StatusLabel.Text =
+                $"kontroll={CheckControl.IsChecked}, loop={CheckLoop.IsChecked}, method={CheckMethod.IsChecked}";
+        }
+    }
+}
